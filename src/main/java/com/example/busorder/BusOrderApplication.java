b755a704925cdf3d362656b14fbf3798483e0057
Siarhey Kulazhanka <skulazhanka@exadel.com>
2023-07-24 18:09:50 +0200

    init project

---
+++ src/main/java/com/example/busorder/BusOrderApplication.java
@@ -0,0 +1,14 @@
+package com.example.busorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusOrderApplication.class, args);
+        System.out.println("run test");
+    }
+
+}
---
+++ src/test/java/com/example/busorder/BusOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.busorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
