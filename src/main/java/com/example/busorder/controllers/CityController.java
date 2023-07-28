94f292657d598df8bfac7da94c770cdb1c4be043
Siarhey Kulazhanka <skulazhanka@exadel.com>
2023-07-28 08:12:48 +0200

    feat: Add CityService

--- src/main/java/com/example/busorder/controllers/CityController.java
+++ src/main/java/com/example/busorder/controllers/CityController.java
@@ -1,9 +1,11 @@
 package com.example.busorder.controllers;
 
+import com.example.busorder.service.CityService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "City")
 @RestController()
 @RequestMapping("/api")
+@AllArgsConstructor
 public class CityController {
+
+    private final CityService cityService;
 
     @ApiResponse(responseCode = "200", description = "Cities are returned")
     @Operation(summary = "city", description = "Get available cities")
     @GetMapping("/city")
     public String getAllCities() {
-        return "Minsk";
+        return cityService.getCity();
+
     }
 }
---
+++ src/main/java/com/example/busorder/service/CityService.java
@@ -0,0 +1,11 @@
+package com.example.busorder.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CityService {
+
+    public String getCity() {
+        return "Homel";
+    }
+}
