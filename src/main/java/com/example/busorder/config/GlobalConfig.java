0d8648da151251f81c681bcd57427624d509b96c
Siarhey Kulazhanka <skulazhanka@exadel.com>
2023-07-30 10:49:59 +0200

    chore: Add swager adjusments

--- src/main/java/com/example/busorder/config/GlobalConfig.java
+++ src/main/java/com/example/busorder/config/GlobalConfig.java
@@ -5,9 +5,9 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(info = @Info(
-        title = "Data Enrichment service",
-        description = "Data Enrichment API"))
+        title = "Order bus",
+        description = "Order Bus API"))
 @Configuration
 public class GlobalConfig {
-    public static final String DEFAULT_API_SECURITY_SCHEMA_NAME = "JWT";
+
 }
--- src/main/java/com/example/busorder/controllers/RouteController.java
+++ src/main/java/com/example/busorder/controllers/RouteController.java
@@ -3,6 +3,8 @@ package com.example.busorder.controllers;
 
 import com.example.busorder.models.entities.Route;
 import com.example.busorder.service.RouteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +21,9 @@ public class RouteController {
 
     private final RouteService routeService;
 
+
+    @ApiResponse(responseCode = "200", description = "Available user routes")
+    @Operation(summary = "route", description = "Route destination")
     @GetMapping("/routes")
     List<Route> getAllRoute() {
         return routeService.getAllRoutes();
