53f77d7668b0414f2dac41a9d56ba4b344e889ce
Siarhei <siarhkul@gmail.com>
2023-08-06 21:24:24 +0200

    add todo

--- src/main/java/com/example/busorder/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/busorder/exceptions/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+//todo do validation if route is exiting in the DB
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
