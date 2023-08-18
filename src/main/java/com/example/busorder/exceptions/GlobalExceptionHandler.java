4cfb9e33aebe9dc8059d17f7133d34f71a8fff60
Siarhei <siarhkul@gmail.com>
2023-08-18 19:58:49 +0200

    Add date validation

--- src/main/java/com/example/busorder/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/busorder/exceptions/GlobalExceptionHandler.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-//todo do validation if route is exiting in the DB
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
--- src/main/java/com/example/busorder/models/dto/BookingRequestDTO.java
+++ src/main/java/com/example/busorder/models/dto/BookingRequestDTO.java
@@ -1,6 +1,7 @@
 package com.example.busorder.models.dto;
 
 
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
@@ -15,7 +16,8 @@ import java.util.Date;
 @Builder
 @ToString
 public class BookingRequestDTO {
-//todo add Date validation
+
+    @Future(message = "The date must be in the future.")
     private Date date;
 
     @NotEmpty( message = "Departure city is not be empty")
