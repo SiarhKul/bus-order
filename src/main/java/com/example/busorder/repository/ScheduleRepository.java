4880f63c4b8f803f8aa1b58f312b922699ff5ba9
Siarhei <siarhkul@gmail.com>
2023-08-24 22:29:29 +0200

    feat: Update SchedulerRepository

--- src/main/java/com/example/busorder/repository/ScheduleRepository.java
+++ src/main/java/com/example/busorder/repository/ScheduleRepository.java
@@ -5,24 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.sql.ResultSet;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Integer> {
 
-/*    @Query(value = "select s.date from Schedule s " +
-            "join Route r on r.id = s.id " +
-            "where s.amountPassengers=?1")
-    List<LocalDate> getSchedule(Integer people);*/
-
-
-    @Query(value = "select s.date from Schedule s " +
-            "join Route r on r.id = s.id " +
-            "where s.amount_passengers=?1", nativeQuery = true)
-    List<ResultSet> getSchedule(Integer people);
+    @Query(value = "select s from Schedule s " +
+            "where s.route.departure_city=:departureCity and s.route.destination_city=:destinationCity")
+    List<Schedule> getSchedule(String departureCity, String destinationCity);
 
 }
--- src/main/java/com/example/busorder/service/serviceInterfaces/ScheduleImpl.java
+++ src/main/java/com/example/busorder/service/serviceInterfaces/ScheduleImpl.java
@@ -6,10 +6,7 @@ import com.example.busorder.repository.ScheduleRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.sql.ResultSet;
-import java.time.LocalDate;
 import java.util.List;
-
 
 @Service
 @AllArgsConstructor
@@ -19,16 +16,11 @@ public class ScheduleImpl implements ScheduleService {
 
     @Override
     public List<Schedule> getSchedule(TripDTO tripDTO) {
+        String departureCity = tripDTO.getDepartureCity();
+        String destinationCity = tripDTO.getDestinationCity();
 
-        Schedule build = Schedule.builder().id(100).amountPassengers(100).build();
-        List<Schedule> build1 = List.of(build);
-//        LocalDate localDate = LocalDate.parse("2024-02-01");
-//        TripDTO build = TripDTO.builder().date(localDate).build();
+        List<Schedule> schedules = scheduleRepository.getSchedule(departureCity, destinationCity);
 
-        List<ResultSet> schedule = scheduleRepository.getSchedule(1);
-
-        return build1;
-
-//        return scheduleRepository.findAll();
+        return schedules;
     }
 }
